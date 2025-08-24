f07849096e5896094bba6e4c8d12e36ae5ee8a06
HamSangEok <hamsoo159@naver.com>
2025-08-25 04:05:01 +0900

    fxi : builderr

--- fsd/widgets/hero/lib/constants.ts
+++ fsd/widgets/hero/lib/constants.ts
@@ -2,8 +2,5 @@ import { TechStackOption } from "../model/type";
 
 export const TECH_STACK_OPTIONS: TechStackOption[] = [
   { id: "React", label: "React", value: "React" },
-  { id: "NextJs", label: "NextJs", value: "NextJs" },
   { id: "JavaScript", label: "JavaScript", value: "JavaScript" },
-  { id: "TypeScript", label: "TypeScript", value: "TypeScript" },
-  { id: "Computer Science", label: "Computer Science", value: "Computer Science" },
 ] as const;
